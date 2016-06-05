ec408dc3f288693e3e625cc91118295af5b8b578
Joe <joe@iachieved.it>
2016-06-05 16:25:28 -0500

    Move out example client

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "MQTT",
     dependencies:[
-	    .Package(url: "https://github.com/VeniceX/TCP", majorVersion:0, minor:8)
+      .Package(url:"https://github.com/VeniceX/TCP", majorVersion:0, minor:8)
     ]
 )
